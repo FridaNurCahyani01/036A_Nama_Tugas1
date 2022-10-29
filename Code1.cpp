0663bd209fc916e4adf577e52f2a976652a7a77b
FridaNurCahyani01 <116931132+FridaNurCahyani01@users.noreply.github.com>
2022-10-29 09:35:03 +0700

    Add files via upload

---
+++ Code1.cpp
@@ -0,0 +1,16 @@
+  #include<stdio.h>
+
+int main(){
+	
+	int angkaBulat = 10;
+	float angkaKoma = 1.5f;
+	char karakter = 'a';
+	char string[100] = "Frida";
+	
+	printf("%d\n", angkaBulat);
+	printf("%.2f\n", angkaKoma);
+	printf("%c\n", karakter);
+	printf("%s\n", string);                                                                                                                                                                                                                                                            
+	
+	return 0;
+}                                                                                                                                      
